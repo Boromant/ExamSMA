b3f20c263afc590862c07d91f1b9a37ca7877903
Boromant <anthony.michallet42@gmail.com>
2023-01-06 12:51:14 +0100

    Commit de presque fin de TP

--- sma/main.py
+++ sma/main.py
@@ -24,7 +24,7 @@ class Body:
         self.fustrum = Fustrum(100, self)
 
     def applyDecision(self):
-        # Pour le rebondissement
+        #Rebondissement
         if self.position[0]-self.size <= 0:
             self.position[0] = self.size
             self.v[0] *= -1
@@ -42,87 +42,63 @@ class Body:
             self.position[1] = core.WINDOW_SIZE[1] - self.size
             self.v[1] *= -1
             self.a[1] *= -1
-
         # Décision
         if self.a.length() > self.amax:
             self.a.scale_to_length(self.amax)
-        self.v += self.a
+        if self.a != Vector2(0, 0):
+            self.v = self.a     # On fait ça car bug: tous les body ont le même v
         if self.v.length() > self.vmax:
             self.v.scale_to_length(self.vmax)
+        self.a = Vector2(0, 0)
         self.position += self.v
-
-    def show(self):
-        core.Draw.circle((0, 0, 255), self.position, self.size)
-        core.Draw.line((255, 255, 255), self.position, self.position + self.v)
 
 
 class Agent:
-    def __init__(self, body):
+    def __init__(self, body, status="sain"):
         self.uuid = random.randint(0, 1000000)
         self.body = body
         self.listPerception = []
+        self.status = status
+        self.previousStatus = status
+        self.start = True
+        self.duree = 0
 
-    def filtrePerception(self, listPerception):
-        target = None
-        dtarget = 1000000
+    def filtrePerception(self):
         voisins = []
-        dangers = []
-
-        for p in listPerception:
-            if isinstance(p, Item):
-                distance = self.body.position.distance_to(p.position)
-                if p.type == "creep":
-                    if distance < dtarget:
-                        dtarget = distance
-                        target = p
-                else:
-                    dangers.append(p)
-            else:
-                voisins.append(p)
-        return voisins, target, dangers
+        for p in self.listPerception:
+            voisins.append(p)
+        return voisins
 
     def doDecision(self):
-        voisins, target, dangers = self.filtrePerception(self.listPerception)
-        rep = Vector2(0, 0)
-        att = Vector2(0, 0)
-        if target is None and self.body.v.length() == 0:
-            att += Vector2(random.randint(-5, 5), random.randint(-5, 5))
-        else:
-            for v in voisins:
-                if v.body.size > self.body.size:
-                    rep += self.body.position - v.body.position
-                else:
-                    att += v.body.position - self.body.position
-            if len(voisins) != 0:
-                rep /= len(voisins)
-                att /= len(voisins)
-
-            if target is not None:
-                att += target.position - self.body.position
-
-        for danger in dangers:
-            d = self.body.position - danger.position
-            rep += d
-        self.body.a = rep + att
+        if self.start:
+            self.start = False
+            self.body.a = Vector2(random.randint(-5, 5), random.randint(-5, 5))
+        self.duree += 1
+        if self.status != self.previousStatus:
+            self.duree = 0
+            self.previousStatus = self.status
+        if self.status == "mort":
+            self.body.a = Vector2(0, 0)
+            self.body.v = Vector2(0, 0)
 
     def show(self):
-        self.body.show()
+        match self.status:
+            case "sain":
+                couleur = (0, 255, 0)
+            case "infecté":
+                couleur = (255, 0, 0)
+            case "mort":
+                couleur = (0, 0, 0)
+            case "quarantaine":
+                couleur = (0, 0, 255)
+            case _:
+                couleur = (255, 255, 255)
+        core.Draw.circle(couleur, self.body.position, self.body.size)
 
 
-class Item:
-    def __init__(self, position, type):
-        self.position = position
-        self.type = type
-        if self.type == "obstacle":
-            self.size = 20
-        elif self.type == "creep":
-            self.size = 3
-
-    def show(self):
-        if self.type == "obstacle":
-            core.Draw.circle((0, 255, 0), self.position, self.size)
-        elif self.type == "creep":
-            core.Draw.circle((255, 0, 0), self.position, self.size)
+class Epidemie:
+    def __init__(self, dico):
+        self.dico = dico
 
 
 class Environnement:
@@ -131,13 +107,11 @@ class Environnement:
         self.H = core.WINDOW_SIZE[1]
         self.listAgent = []
         self.listItem = []
+        self.epidemie = None
 
     def computePerception(self):
         for agent in self.listAgent:
             agent.listPerception = []
-            for item in self.listItem:
-                if agent.body.fustrum.inside(item.position):
-                    agent.listPerception.append(item)
             for a in self.listAgent:
                 if agent.body.fustrum.inside(a.body.position) and a.uuid != agent.uuid:
                     agent.listPerception.append(a)
@@ -149,26 +123,41 @@ class Environnement:
     def applyDecision(self):
         for agent in self.listAgent:
             agent.body.applyDecision()
-            for item in self.listItem:
-                if agent.body.position.distance_to(item.position) < agent.body.size + item.size:
-                    if item.type == "creep":
-                        self.delItem(item)
-                        agent.body.size += 1
-                        agent.body.vmax -= 0.1
-                    else:
-                        self.delAgent(agent)
+
+    def update(self):
+        dico = self.epidemie.dico
+        for a1 in self.listAgent:
+            if a1.status == "infecté":
+                for a2 in self.listAgent:
+                    if a1.uuid != a2.uuid and a2.status == "sain":
+                        if a1.body.position.distance_to(a2.body.position) < a1.body.size + a2.body.size + dico["distContagion"]:
+                            if dico["pContagion"] > random.random() and a1.duree > dico["dContagion"]:
+                                a2.status = "infecté"
+                                break
+                if a1.duree > dico["dDeces"]:
+                    if dico["pDeces"] > random.random():
+                        a1.status = "mort"
+                        continue
+
+
+
 
     def show(self):
+        s, i, d = 0, 0, 0
         for agent in self.listAgent:
             agent.show()
-        for item in self.listItem:
-            item.show()
+            if agent.status == "sain":
+                s += 1
+            elif agent.status == "infecté":
+                i += 1
+            elif agent.status == "mort":
+                d += 1
+        n = len(self.listAgent)
+        print("Sain: ", s/n*100, "%", "Infecté: ", i/n*100, "%", "Mort: ", d/n*100, "%")
+
 
     def addAgent(self, agent):
         self.listAgent.append(agent)
-
-    def addItem(self, item):
-        self.listItem.append(item)
 
     def addRandomAgents(self, n):
         size = 10
@@ -176,28 +165,8 @@ class Environnement:
             a = Agent(Body(Vector2(random.randint(size, self.L-size), random.randint(size, self.H-size)), size))
             self.listAgent.append(a)
 
-    def addRandomObstacles(self, n):
-        size = 20
-        for i in range(n):
-            o = Item(Vector2(random.randint(size, self.L-size), random.randint(size, self.H-size)), "obstacle")
-            self.listItem.append(o)
-
-    def addRandomCreeps(self, n):
-        size = 3
-        for i in range(n):
-            c = Item(Vector2(random.randint(size, self.L-size), random.randint(size, self.H-size)), "creep")
-            self.listItem.append(c)
-            for item in self.listItem:
-                if c.position.distance_to(item.position) < c.size + item.size and item != c:
-                    self.delItem(c)
-                    n += 1
-                    break
-
     def delAgent(self, agent):
         self.listAgent.remove(agent)
-
-    def delItem(self, item):
-        self.listItem.remove(item)
 
 
 def setup():
@@ -207,10 +176,9 @@ def setup():
     env = Environnement()
     core.memory("env", env)
 
-    env.addRandomAgents(1)
-    env.addRandomCreeps(50)
-    env.addRandomObstacles(5)
-
+    env.epidemie = Epidemie({"dIncubation": 0, "dContagion": 50, "pContagion": 0.8,
+                             "dDeces": 100, "pDeces": 0.2, "distContagion": 20})
+    env.addRandomAgents(30)
 
 def run():
     core.cleanScreen()
@@ -221,6 +189,21 @@ def run():
     env.computePerception()
     env.computeDecision()
     env.applyDecision()
+    env.update()
+
+    click = core.getMouseLeftClick()
+    if click:
+        pos = click
+        max = 1000
+        amax = None
+        for a in env.listAgent:
+            dist = a.body.position.distance_to(pos)
+            if dist < max:
+                max = dist
+                amax = a
+        amax.status = "infecté"
+
+
 
 
 core.main(setup, run)
